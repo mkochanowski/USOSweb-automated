0b17de679fc8c2c1c6455df1ace09022f570260b
Marek Kochanowski <marek@kochanow.ski>
2018-04-28 23:33:58 +0200

    Add default scraping templates

---
+++ templates/scraping/dla_stud-studia-oceny-index.py
@@ -0,0 +1,28 @@
+import logging
+from bs4 import BeautifulSoup
+
+logging = logging.getLogger(__name__)
+
+
+class ScrapingTemplate:
+    def __init__(self, web_driver: object) -> None:
+        self.driver = web_driver
+        self.results = None
+
+    def get_data(self) -> object:
+        self._parse(soup=self._soup())
+
+        return self.results
+
+    def _soup(self) -> object:
+        tree = self.driver.find_element_by_id("tab1")
+        soup = BeautifulSoup(
+            tree.get_attribute("innerHTML"),
+            "html.parser")
+    
+        return soup
+
+    def _parse(self, soup: object) -> None:
+        self.results = {
+            "module": __name__
+        }
---
+++ templates/scraping/dla_stud-studia-sprawdziany-index.py
@@ -0,0 +1,43 @@
+import logging
+from bs4 import BeautifulSoup
+
+logging = logging.getLogger(__name__)
+
+
+class ScrapingTemplate:
+    def __init__(self, web_driver: object) -> None:
+        self.driver = web_driver
+        self.results = None
+
+    def get_data(self) -> object:
+        self._parse(soup=self._soup())
+
+        return self.results
+
+    def _soup(self) -> object:
+        tree = self.driver.find_element_by_id("lista")
+        soup = BeautifulSoup(
+            tree.get_attribute("innerHTML"),
+            "html.parser")
+    
+        return soup
+
+    def _parse(self, soup: object) -> None:
+        helper = Helper(soup=soup, web_driver=self.driver)
+        self.results = {
+            "module": __name__,
+            "new_destinations": helper.get_destinations()
+        }
+
+class Helper:
+    def __init__(self, web_driver: object, soup: object) -> None:
+        self.soup = soup
+        self.driver = web_driver
+
+    def get_destinations(self) -> list:
+        links = self.driver.find_elements_by_class_name("fwdlink")
+        destinations = []
+        for link in links:
+            destinations.append(link.get_attribute("href"))
+
+        return destinations
