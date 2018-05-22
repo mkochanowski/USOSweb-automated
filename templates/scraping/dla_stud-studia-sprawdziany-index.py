e7c4d95158009ebe5d2287f2445b0a25c2044ad9
Marek Kochanowski <marek@kochanow.ski>
2018-05-22 21:17:12 +0200

    Update ScrapingTemplates with minimal docstrings

--- templates/scraping/dla_stud-studia-sprawdziany-index.py
+++ templates/scraping/dla_stud-studia-sprawdziany-index.py
@@ -5,17 +5,22 @@ logging = logging.getLogger(__name__)
 
 
 class ScrapingTemplate:
+    """Scrapes the specific type of page by using predefined
+    set of actions."""
     def __init__(self, web_driver: object) -> None:
         self.driver = web_driver
         self.results = None
 
     def get_data(self) -> object:
+        """Returns scraped and parsed data."""
         self._parse(soup=self._soup())
 
         logging.debug(self.results)
         return self.results
 
     def _soup(self) -> object:
+        """Generates a soup object out of a specific element
+        provided by the web driver."""
         tree = self.driver.find_element_by_id("lista")
         soup = BeautifulSoup(
             tree.get_attribute("innerHTML"),
@@ -24,6 +29,7 @@ class ScrapingTemplate:
         return soup
 
     def _parse(self, soup: object) -> None:
+        """Initializes parsing of the innerHTML."""
         parser = Parser(soup=soup, web_driver=self.driver)
         self.results = {
             "module": __name__,
@@ -31,11 +37,13 @@ class ScrapingTemplate:
         }
 
 class Parser:
+    """Parses the provided HTML with BeautifulSoup."""
     def __init__(self, web_driver: object, soup: object) -> None:
         self.soup = soup
         self.driver = web_driver
 
     def get_destinations(self) -> list:
+        """Returns the results back to the ScrapingTemplate."""
         links = self.driver.find_elements_by_class_name("fwdlink")
         destinations = []
         for link in links:
--- templates/scraping/dla_stud-studia-sprawdziany-pokaz.py
+++ templates/scraping/dla_stud-studia-sprawdziany-pokaz.py
@@ -6,16 +6,21 @@ logging = logging.getLogger(__name__)
 
 
 class ScrapingTemplate:
+    """Scrapes the specific type of page by using predefined
+    set of actions."""
     def __init__(self, web_driver: object) -> None:
         self.driver = web_driver
         self.results = None
 
     def get_data(self) -> object:
+        """Returns scraped and parsed data."""
         self._parse(soup=self._soup())
 
         return self.results
 
     def _soup(self) -> object:
+        """Generates a soup object out of a specific element
+        provided by the web driver."""
         tree = self.driver.find_element_by_id("layout-c22a")
         soup = BeautifulSoup(
             tree.get_attribute("innerHTML"),
@@ -24,6 +29,7 @@ class ScrapingTemplate:
         return soup
 
     def _parse(self, soup: object) -> None:
+        """Initializes parsing of the innerHTML."""
         parser = Parser(soup=soup, web_driver=self.driver)
         self.results = {
             "module": __name__,
@@ -32,6 +38,7 @@ class ScrapingTemplate:
 
 
 class Parser:
+    """Parses the provided HTML with BeautifulSoup."""
     def __init__(self, web_driver: object, soup: object) -> None:
         self.soup = soup
         self.driver = web_driver
@@ -41,6 +48,7 @@ class Parser:
         self._tree_entries = []
 
     def get_parsed_results(self) -> list:
+        """Returns the results back to the ScrapingTemplate."""
         self._parse_groups(self.soup)
         tree = self.soup.find("div", {"id": "drzewo"})
         tree = tree.find("div", id=True, recursive=False)
